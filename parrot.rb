e7789e202bb768e6f9670c84e05c30a1bd94d8bd
Troy Gooden <tro12185@gmail.com>
2018-12-19 04:42:34 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(Squawk!)
-  puts Squack
-  return Squawk!
+def parrot(arg)
+  puts arg
+  return arg
 end  
